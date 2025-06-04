389a2f975c79ab133303402eaa6b8c415a1f6090
abreumrts <saraabreumartins2008@gmail.com>
2025-06-04 20:14:25 -0300

    update

--- logica.js
+++ logica.js
@@ -1,10 +1,52 @@
-botaoInicar = document.querySelector(".iniciar")
-
+listaPerguntas = [
+    
+    {
+        pergunta: "Qual o maior ser vivo?",
+        opcoes: [
+            "baleia azul",
+            "fungo de mel",
+            "elefante",
+            "girafa"
+        ],
+        correta: 1
+    },
+    {
+        pergunta: "Qual a primeira capital do Brasil?",
+        opcoes: [
+            "Groaíras",
+            "Taquara",
+            "Salvador",
+            "Rio de Janeiro"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: "Em que ano o Brasil ganhou sua segunda Copa do Mundo?",
+        opcoes: [
+            "1974",
+            "1970",
+            "1966",
+            "1962"
+        ],
+        correta: 3
+    },
+    ]
+    
+    pergunta = null
+botaoIniciar = document.querySelector(".iniciar")
 botaoIniciar.addEventListener("click",iniciarJogo)
 
 function iniciarJogo() {
-  telaIncial = document.querySelector(".tela-inicial")
+  telaInicial = document.querySelector(".tela-inicial")
   telaInicial.style.display = "none"
   telaPerg = document.querySelector(".tela-pergunta")
   telaPerg.style.display = "block"
+  
+  mostrarPergunta()
+}
+
+function mostrarPergunta(){
+    pergunta = listaPerguntas[0]
+    
+    console.log(pergunta)
 }
